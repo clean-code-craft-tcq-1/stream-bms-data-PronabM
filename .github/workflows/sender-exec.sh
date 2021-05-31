7b0232b66707a4fa8b41553a0fe0849728615395
Pronab Mukherjee <pronab.mukherjee.96@gmail.com>
2021-05-31 21:46:54 +0530

    Update sender-exec.sh

--- .github/workflows/sender-exec.sh
+++ .github/workflows/sender-exec.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 export JAVA_PROGRAM_ARGS=`echo "$@"`
-mvn exec:java -Dexec.mainClass="bmsstream.Sender" -Dexec.args="$JAVA_PROGRAM_ARGS"
+mvn compile exec:java -Dexec.mainClass="bmsstream.Sender" -Dexec.args="$JAVA_PROGRAM_ARGS"
