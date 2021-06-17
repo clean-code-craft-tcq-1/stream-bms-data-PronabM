083e736df0debf899f4231bd1fd85ad385dc00b0
venkateshnichenametla <77480058+venkateshnichenametla@users.noreply.github.com>
2021-06-17 20:33:18 +0530

    Removed empty lines

--- Receiver/BatteryDataStreamingReceiver/Temperature.cs
+++ Receiver/BatteryDataStreamingReceiver/Temperature.cs
@@ -3,21 +3,17 @@
     public class Temperature
     {
         private double _minimum = double.MaxValue;
-
         public double Minimum
         {
             get { return _minimum; }
             set { _minimum = value; }
         }
-
         private double _maximum = double.MinValue;
-
         public double Maximum
         {
             get { return _maximum; }
             set { _maximum = value; }
         }
-
         public double MovingAverage { get; set; }
     }
 }
