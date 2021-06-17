8967be77413ec651cebef10a247c3192b19b25d3
venkateshnichenametla <77480058+venkateshnichenametla@users.noreply.github.com>
2021-06-17 20:32:54 +0530

    Removed empty lines

--- Receiver/BatteryDataStreamingReceiver/StateOfCharge.cs
+++ Receiver/BatteryDataStreamingReceiver/StateOfCharge.cs
@@ -2,23 +2,18 @@
 {
     public class StateOfCharge
     {
-
         private double _minimum = double.MaxValue;
-
         public double Minimum
         {
             get { return _minimum; }
             set { _minimum = value; }
         }
-
         private double _maximum = double.MinValue;
-
         public double Maximum
         {
             get { return _maximum; }
             set { _maximum = value; }
         }
-
         public double MovingAverage { get; set; }
     }
 }
