c55e3469dada1ad6cee841e03fa6456bd1a9e06c
venkateshnichenametla <77480058+venkateshnichenametla@users.noreply.github.com>
2021-06-17 20:16:19 +0530

    Delete BatteryParameter.cs

--- BatteryParameter.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace BatteryDataStreamingReceiver
-{
-    public class BatteryParameter
-    {
-        public double Temperature { get; set; }
-        public double StateOfCharge { get; set; }
-    }
-}
