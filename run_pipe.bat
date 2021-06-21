13c498a21fff4a59913cecf37a6998e4dc30ef6e
venkateshnichenametla <77480058+venkateshnichenametla@users.noreply.github.com>
2021-06-21 18:56:23 +0530

    Update run_pipe.bat

--- run_pipe.bat
+++ run_pipe.bat
@@ -1,5 +1,5 @@
 pushd "%~dp0"
 
-java -ea -cp "src;lib\*" main.java.bmsstream.Sender "R" "20" | dotnet run --project src\Receiver\Receiver\Receiver.csproj
+java -ea -cp "src" main.java.bmsstream.Sender "R" "20" | dotnet run --project src\Receiver\Receiver\Receiver.csproj
 
 popd
