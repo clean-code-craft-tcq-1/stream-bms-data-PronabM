9785e1a4137ab03ba3670c7bfb0e8ce3b18425da
venkateshnichenametla <77480058+venkateshnichenametla@users.noreply.github.com>
2021-06-21 21:14:33 +0530

    Update run_pipe.bat

--- run_pipe.bat
+++ run_pipe.bat
@@ -1,6 +1,9 @@
 pushd "%~dp0"
 
 cd src\main\java
+
+javac bmsstream\Sender.java
+
 java bmsstream.Sender "R" "20" | dotnet run --project Receiver\BatteryDataStreamingReceiver\BatteryDataStreamingReceiver.csproj
 
 popd
